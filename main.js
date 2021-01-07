66bf3c1f8427fd64da3327b42b28121e68445557
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-01-07 08:39:46 +0100

    script v2

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
+// Selectors
 const input = document.getElementById("pass");
 const div = document.querySelector(`.message`);
-const password = "user";
-const message = "Jutro czeka Cię dobry dzień!";
 
+// Function arrays
+const passwords = ["user", "wiosna", "Ania"];
+const messages = [
+  "wyjechałam na zawsze",
+  "piękna pora roku",
+  "fajny styl masz",
+];
+
+// Input function
 input.addEventListener("input", (e) => {
-  //   console.log(e.target.value);
-  if (password === e.target.value) {
-    div.textContent = message;
-    e.target.value = "";
-  } else {
-    div.textContent = "";
-  }
+  //   console.log(e.target);
+  //   console.log(this);
+
+  div.textContent = "";
+
+  const text = e.target.value;
+
+  passwords.forEach((password, index) => {
+    if (password === text) {
+      div.textContent = messages[index];
+      e.target.value = "";
+    }
+  });
 });
 
+// Focus function
 input.addEventListener(`focus`, (e) => {
   e.target.classList.add("active");
 });
 
+// Blur function
 input.addEventListener(`blur`, (e) => {
   e.target.classList.remove("active");
 });
